8e8f9ead592dbc576d1abc9c9944e79917623350
Rendani-Tshishonga <rendanijunior20@gmail.com>
2025-02-16 21:33:02 +0200

    mine

--- node.py
+++ node.py
@@ -16,12 +16,48 @@ blockchain = Blockchain()
 """ Create a route to mine a new block """
 @app.route("/mine", methods=['GET'])
 def mine():
-    return "We'll mine a new block"
+    """ We run a proof of work algorithm to get the next proof """
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    """ Reward for finding the proof """
+    """ The sender is "0" to signify that this node has mined a new coin """
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    """ Forge the new block by adding it to the chain """
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transaction': block['transaction'],
+        'proof': block['proof'],
+        'previous_hash' block['previous_hash'],
+    }
+
+    return jsonify(response), 200
 
 """ Create a route to create a new transaction """
 @app.route("/transactions/new", methods=['POST'])
 def new_transaction():
-    return "We'll add a new transaction"
+    values = request.get_json()
+
+    """ Check that required fields are in the POST data """
+    required = ["sender", "recipient", "amount"]
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    """ Create a new transaction """
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to block {index}')
+    return jsonify(response), 201
 
 """ Create a route to return a blockchain """
 @app.route("/chain", methods=['GET'])
